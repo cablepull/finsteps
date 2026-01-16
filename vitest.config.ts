20bc16b2ee6073b0b9bc7fef76d4b131e20a8ff2
cablepull <135179737+cablepull@users.noreply.github.com>
2026-01-16 13:35:57 -0800

    Fix merged package and vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    environment: "node",
+    environmentMatchGlobs: [["tests/unit/**/*.test.js", "jsdom"]],
     coverage: {
       provider: "v8",
       reporter: ["text", "html"]
